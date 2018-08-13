d24ab5f8f6272a0e669797810cb99331b80dc34d
Lazar Veselinovic <veselinovic93@hotmail.com>
2018-08-13 11:20:35 +0200

    Brandly website full responsive with grid CSS + flexbox

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+const parallax = document.querySelector('#parallax')
+
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset
+    parallax.style.backgroundPositionY = offset * 0.7 + 'px';
+
+})
+
+
+const hamb = document.querySelector('.nav__hamburger')
+
+hamb.addEventListener('click', function () {
+    let menu = document.querySelector('.nav__items')
+    menu.classList.toggle('nav__items--close')
+})
